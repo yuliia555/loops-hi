d2187ba4475c6f803d8eacc05dc8e6c7ff5549bb
yuliia555 <113055649+yuliia555@users.noreply.github.com>
2022-09-21 14:29:35 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    for (let index = 0; index <= 9; index++) {
+        basic.showString("Hi")
+    }
+})
+basic.showIcon(IconNames.Heart)
